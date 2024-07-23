b033e90ae0c988eff17fcaa5cbf6c5928d016c42
nhat thang <nhtthng2812@gmail.com>
2024-07-23 21:30:11 +0700

    add Bai_1_Cau_7 and result

--- Bai_1.cpp
+++ Bai_1.cpp
@@ -69,6 +69,43 @@ void Xuatmang(int a[], int n) {
 	}
 }
 
+int partition(int arr[], int low, int high)
+{
+	
+
+	int pivot = arr[high];
+	
+	
+	int i = (low - 1);
+
+	for (int j = low;j <= high - 1;j++)
+	{
+		
+		if (arr[j] < pivot)
+		{
+			
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[high]);
+	return (i + 1);
+}
+
+void Cau_7(int arr[], int low, int high)
+{
+	
+	if (low < high)
+	{
+
+		int pi = partition(arr, low, high);
+
+		
+		Cau_7(arr, low, pi - 1);
+		Cau_7(arr, pi + 1, high);
+	}
+}
+
 void Cau_6(int a[], int& n) {
 	for (int i = 0; i < n - 1; i++) {
 		int minPosition = i;
@@ -88,7 +125,7 @@ int main() {
 	cin >> n;
 	Cau_1(a, n);
 	Xuatmang(a, n);
-	Cau_6(a, n);
+	Cau_7(a, 0,n-1);
 	cout << "Mang sau khi sap xep la" << endl;
 	Xuatmang(a, n);
 	//Cau_4(a, n);
